01a295710912fa7e3a59b43471ff4522d5cbd31e
Pål Ruud <ruudud@gmail.com>
2013-08-26 13:29:11 +0200

    Print-pdf mods. Add missing higlightjs style file. Add missing logo.

--- Gruntfile.js
+++ Gruntfile.js
@@ -114,8 +114,8 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
 	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
-	grunt.loadNpmTasks( 'grunt-contrib-sass' );
 	grunt.loadNpmTasks( 'grunt-contrib-connect' );
+	grunt.loadNpmTasks( 'grunt-sass' );
 	grunt.loadNpmTasks( 'grunt-zip' );
 
 	// Default task
